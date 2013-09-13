6a17172022bd1c8b886baef1df726cb4bdaa2801
Christopher Roach <vthakr@gmail.com>
2013-09-13 03:20:58 -0700

    Added example NoC I.4

---
+++ introduction/noc_i_4_gaussian.py
@@ -0,0 +1,21 @@
+from processing import *
+
+
+def setup():
+    size(640, 360)
+    background(255)
+
+def draw():
+    # Get a gaussian number w/ mean of 0 and standard deviation of 1.0
+    xloc = randomGaussian()
+
+    sd = 60
+    mean = width/2
+    xloc = (xloc * sd) + mean
+
+    noStroke()
+    fill(0, 10)
+    # rect(xloc, height/2, 16, 16);
+    ellipse(xloc, height/2, 16, 16)
+
+run()
