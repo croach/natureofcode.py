d08e2de8211ddb59bd5d5b212d268c14da053647
Christopher Roach <vthakr@gmail.com>
2013-09-15 16:24:35 -0700

    First commit of noise walker ex. 1.5

---
+++ introduction/noc_i_5_noise_walk/walker.py
@@ -0,0 +1,21 @@
+from random import choice
+
+from processing import *
+
+
+class Walker(object):
+    def __init__(self):
+        self.location = PVector(width/2, height/2)
+        self.noff = PVector(random(1000), random(1000))
+
+    def walk(self):
+        self.location.x = map(noise(self.noff.x), 0, 1, 0, width)
+        self.location.y = map(noise(self.noff.y), 0, 1, 0, height)
+        self.noff.add(0.01, 0.01, 0)
+
+    def display(self):
+        strokeWeight(2)
+        fill(127)
+        stroke(0)
+        ellipse(self.location.x, self.location.y, 48, 48)
+
