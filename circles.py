fc6b120d43ea0ecf44c5c9e81f110b9af27295e6
Christopher Roach <vthakr@gmail.com>
2014-04-13 20:57:36 -0700

    Added a couple more examples

---
+++ circles.py
@@ -0,0 +1,18 @@
+from processing import *
+
+
+def setup():
+    size(323, 200)
+    frameRate(1)
+    background(200)
+
+def draw():
+    stroke(random(255))
+    strokeWeight(random(10))
+    fill(random(255))
+    diam = random(100)
+    x = random(width)
+    y = random(height)
+    ellipse(x, y, diam, diam)
+
+run()
---
+++ flashlight.py
@@ -0,0 +1,19 @@
+from processing import *
+
+
+def setup():
+    size(400, 400)
+    noStroke()
+
+
+def draw():
+    background(230)
+    step = 50
+    for i in range(0, width + step, step):
+        for j in range(0, height + step, step):
+            diameter = dist(mouseX, mouseY, i, j)/3.3
+            fill(0, diameter*4)
+            ellipse(i, j, diameter, diameter)
+
+
+run()
